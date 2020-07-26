040894e7dbec2dcee4a5e4ee4682ea5e5661f4f4
Jeremy Cady <jeremy@mx103.com>
2020-07-26 15:24:18 -0500

    add disabling of activities at same date/time

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const job = document.querySelector('#title');
-const design = document.querySelector('#design');
 const colorSelect = document.querySelector('#color');
 
 // focuses on the first text input field
@@ -40,45 +39,6 @@ const insertDesignNotChosen = () => {
     colorSelect.value = pleaseSelect.value;
 }
 
-// build tshirtColor array with design, colors, and content
-const getColors = () => {
-    const colors = document.querySelector('#color');
-
-    const selectTheme = {
-        design: 'Select Theme',
-        colors: ['please-select'],
-        content: ['Please select a T-shirt theme']
-    };
-
-    tshirtColors.push(selectTheme);
-
-    const jsPuns = {
-        design: 'js puns',
-        colors: [],
-        content: []
-    };
-
-    const heartJs = {
-        design: 'heart js',
-        colors: [],
-        content: []
-    }
-    
-    for (let i=0; i<colors.length; i++) {
-        const color = colors[i];
-        if (color.textContent.includes('(JS Puns shirt only)')) {
-            jsPuns.colors.push(color.value);
-            jsPuns.content.push(color.textContent);
-        } else if (color.textContent.includes('(I ♥ JS shirt only)')) {
-            heartJs.colors.push(color.value);
-            heartJs.content.push(color.textContent);
-        }
-    }
-
-    tshirtColors.push(jsPuns);
-    tshirtColors.push(heartJs);
-}
-
 // hide/unhide colors in color menu
 const changeColor = (colors, include, select) => {
     for (let i=0; i<colors.length; i++) {
@@ -105,6 +65,54 @@ const changeDesign = tshirtSelection => {
     }
 }
 
+// change label opacity and activity disabled
+const disableActivity = (activity, opacity, disable) => {
+    const label = activity.parentNode;
+
+    label.style.opacity = opacity;
+    activity.disabled = disable;
+}
+
+// push checked activites into array to be compared
+const checkActivities = (checkedActivities, activities) => {
+    for (let i=0; i<activities.length; i++) {
+        const activity = activities[i];
+        if (activity.checked) {
+            const activityObject = {
+                name: activity.name,
+                time: activity.attributes[2].value
+            };
+            checkedActivities.push(activityObject);
+        }
+    }
+}
+
+// disables activites that are at the same date and time of activity selected
+const selectActivities = (e) => {
+    const activities = document.querySelectorAll('input[type=checkbox]');
+    const checkedActivities = [];
+
+    for (let i=0; i<activities.length; i++) {
+        const activity = activities[i];
+        disableActivity(activity, 1, false);
+    }
+
+    checkActivities(checkedActivities, activities);
+
+    for (let i=0; i<activities.length; i++) {
+        const activity = activities[i];
+        
+        if (!activity.checked) {
+            for (let i=0; i<checkedActivities.length; i++) {
+                const checkedActivity = checkedActivities[i];
+                if (checkedActivity.time === activity.attributes[2].value) {
+                    disableActivity(activity, .5, true);
+                }
+            }
+        }
+    }
+}
+
 focusFirstInput();
 changeOtherRole();
 insertDesignNotChosen();
@@ -114,7 +122,11 @@ job.addEventListener('change', () => {
     changeOtherRole();
 });
 
-design.addEventListener('change', e => {
+document.querySelector('#design').addEventListener('change', e => {
     const selectDesign = e.target.value;
     changeDesign(selectDesign);
+});
+
+document.querySelector('.activities').addEventListener('change', e=> {
+    selectActivities(e);
 });
