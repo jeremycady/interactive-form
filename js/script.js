822074f3edbc11714dc87b968e3a41fe42a553ef
Jeremy Cady <jeremy@mx103.com>
2020-07-26 06:22:03 -0500

    add tshirt color change function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const jobRole = document.querySelector('#title');
+const tshirtDesign = document.querySelector('#design');
+const tshirtColors = document.querySelector('#color');
 
 // focuses on the first text input field
 const focusFirstInput = () => {
@@ -26,9 +28,34 @@ const changeOtherRole = () => {
     }
 }
 
+//change TShirt Color Menu
+const changeTshirtColor = () => {
+    const tshirtColorSelection = document.querySelector('#color');
+    tshirtColorSelection.innerHTML = '';
+
+    if (tshirtDesign.value === 'js puns') {
+        for (let i=0; i<3; i++) {
+            const color = tshirtColors[i];
+            tshirtColorSelection.innerHTML += color;
+        }
+    } else if (tshirtDesign.value === 'heart js') {
+        for (let i=3; i<6; i++) {
+            const color = tshirtColors[i];
+            tshirtColorSelection.innerHTML += color;
+        }
+    } else {
+        tshirtColorSelection.innerHTML = '<option>Please select a T-shirt theme</option>';
+    }
+}
+
 focusFirstInput();
 changeOtherRole();
+// changeTshirtColor();
 
 jobRole.addEventListener('change', () => {
     changeOtherRole();
+});
+
+tshirtDesign.addEventListener('change', () => {
+    changeTshirtColor();
 });
